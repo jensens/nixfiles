c04ab1afb21f37405c4c50884ef8811d9e47a504
Jens W. Klein <jk@kleinundpartner.at>
2016-02-09 10:13:02 +0100

    more...

--- x11_wm_de.nix
+++ x11_wm_de.nix
@@ -5,7 +5,7 @@
   services.xserver.xkbOptions = "eurosign:e";
 
   # Enable the KDE Desktop Environment.
-  services.xserver.displayManager.lightdm.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
   services.xserver.windowManager.i3.enable = true;
-  services.xserver.desktopManager.kde4.enable = true;
+  services.xserver.desktopManager.gnome3.enable = true;
 }
